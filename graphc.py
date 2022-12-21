34e58166f6c70fb9748ba4b14350e006ed4e64a5
Daxya <74474934+Dat404@users.noreply.github.com>
2022-12-22 00:59:44 +0300

    file

--- graphc.py
+++ graphc.py
@@ -6,7 +6,7 @@ def stats(wins):
     ax.plot(wins, 'o-')
     ax.tick_params(labelcolor = 'r')
     ax.set_xlabel('Attempts', color = 'r')
-    ax.set_ylabel('Number of guessed', color = 'r')
+    ax.set_ylabel('Num of guessed', color = 'r')
     ax.set(facecolor = 'black')
     f.set(facecolor = 'black')
     return plt.show()
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 wins = answ = []
 
 while -1 not in answ: 
-    answ = list(map(int, input('Enter 5 numbers(0 to 5), -2 for stats and exit or -1 for exit/> ').split()))    
+    answ = list(map(int, input('Enter 5 numbers(0 to 5), [-2] for stats and exit or [-1] for exit/> ').split()))    
     wins.append(sum(elm[0] is elm[1] for elm in zip(list(random.sample(range(5), 5)), answ)))
     
     print('U win(times) ', wins[-1])
